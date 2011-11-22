1fed01034b2ac51a2b7038e062b4a628e7a15c1d
Jake Nielsen <jake@jake-Satellite-L450.(none)>
2011-11-22 18:02:40 -0500

    More installer changes

--- installer.sh
+++ installer.sh
@@ -16,9 +16,9 @@ patch <../serjtag-0.3/avrdude-serjtag/src/avrdude-5.3.1-serjtag.patch
 patch <../serjtag-0.3/avrdude-serjtag/src/avrdude-5.3.1-ft245r.patch
 patch <../serjtag-0.3/avrdude-serjtag/src/avrdude-5.3.1-baud.patch
 
-cp ../libftd2xx1.0.4/ftd2xx.h ./
-cp ../libftd2xx1.0.4/WinTypes.h ./
-cp ../libftd2xx1.0.4/build/i386/libftd2xx.a ./
+cp ../libftd2xx0.4.16/ftd2xx.h ./
+cp ../libftd2xx0.4.16/WinTypes.h ./
+cp ../libftd2xx0.4.16/static_lib/libftd2xx.a.0.4.16 ./
 
 ./configure
 
