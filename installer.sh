96ed5ba8d4287fb51699be26ec6303ac92848d27
Jake Nielsen <jake@jake-Satellite-L450.(none)>
2011-11-23 12:28:34 -0500

    beginning of the installer

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Running this installer will result in a modded version of avrdude such that your arduino bitbang programmer will work!
+
+sudo apt-get install patch build-essential libreadline-dev libncurses-dev libusb-dev libftdi-dev automake autoconf bison flex
+sudo apt-get build-dep avrdude avrdude-doc
+
+# For 64-bit:
+#wget http://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx0.4.16_x86_64.tar.gz
+# For 32-bit:
+wget http://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx0.4.16.tar.gz
+
