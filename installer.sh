db2fb6cc55bfde185172a84aff73209c7d579236
Jake Nielsen <jake@jake-Satellite-L450.(none)>
2011-11-23 12:35:19 -0500

    Put should-be finishing touches on installer.sh

--- installer.sh
+++ installer.sh
@@ -10,3 +10,24 @@ sudo apt-get build-dep avrdude avrdude-doc
 # For 32-bit:
 wget http://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx0.4.16.tar.gz
 
+tar xzf avrdude-5.10.tar.gz
+tar xzf libftd2xx*.tar.gz
+cd avrdude-5.10
+for file in ../patch-*.diff; do patch -p0 < $file; done
+cp ../libftd2xx*/static_lib/* .
+cp ../libftd2xx*/*.h .
+cp ../libftd2xx*/*.cfg .
+./configure CFLAGS="-g -O2 -DSUPPORT_FT245R" LIBS="./libftd2xx.a.0.4.16 -lpthread -ldl -lrt"
+make
+
+echo '
+programmer
+  id    = "ftdi";
+  desc  = "SparkFun FTDI Basic Breakout";
+  type  = ft245r;
+  miso  = 1;  # RXD          
+  sck   = 3;  # CTS
+  mosi  = 0;  # TXD
+  reset = 4;  # DTR              
+;' >> avrdude.conf;
+
